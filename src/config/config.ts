68c0ec307a97a5233a89594b7910b4e5b80cb995
kenresoft <amadikenneth8@gmail.com>
2024-10-22 11:57:16 +0100

    Update src/config/config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const config = {
     port: process.env.PORT || 5000,
-    jwtSecret: process.env.JWT_SECRET || 'your-secret-key',
+    jwtSecret: process.env.JWT_SECRET || 'secret-key',
     jwtExpiration: process.env.JWT_EXPIRATION || '1h',
     bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS || '10'),
     mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/authAPI',
