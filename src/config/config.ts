71b35e2f35cf545eb4dabdab8b90c28bfd731991
kenresoft <amadikenneth8@gmail.com>
2024-10-22 21:41:53 +0100

    Update cofig with refresh token defaults

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {
+export default {
     port: process.env.PORT || 5000,
-    jwtSecret: process.env.JWT_SECRET || 'secret-key',
-    jwtExpiration: process.env.JWT_EXPIRATION || '1h',
+    jwtSecret: process.env.JWT_SECRET || 'ACCESS_TOKEN_SECRET',
+    jwtExpiration: '15m',
+    refreshTokenSecret: process.env.JWT_REFRESH_SECRET || 'REFRESH_TOKEN_SECRET',
+    refreshTokenExpiration: '7d',
+    refreshTokenExpirationMs: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     bcryptSaltRounds: parseInt(process.env.BCRYPT_SALT_ROUNDS || '10'),
     mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/authAPI',
 };
