08bb3c565bcf6bf0b17efa7a8721d760f36f503c
kenresoft <amadikenneth8@gmail.com>
2024-10-22 21:37:53 +0100

    Update `src/app.ts` and `src/server.ts`

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import authRoutes from './src/routes/authRoutes';
-import { config } from './src/config/config';
+import routes from './routes/router';
+import config  from './config/config';
 
 const app = express();
 
 app.use(express.json());
 
 // Using API versioning for all routes
-app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/', routes());
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
 
 mongoose.connect(config.mongoURI, {})
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('MongoDB connection error:', error));
 
-export default app;
+export default app; 
